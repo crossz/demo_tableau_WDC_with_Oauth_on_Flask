338fb06ab3f748d5bd4c05f9e25437be7718cbbc
crossz <ukcrossa@gmail.com>
2022-10-12 10:37:07 +0800

    :rocket: cs_p0 on heroku + auth0, version 1.

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from os import environ as env
 from werkzeug.middleware.dispatcher import DispatcherMiddleware # use to combine each Flask app into a larger one that is dispatched based on prefix
 from werkzeug.serving import run_simple
 import api
@@ -10,5 +11,7 @@ application = DispatcherMiddleware(flask_app_1, {
     '/api/v2': flask_app_2
 })
 
+port = int(env.get("PORT", 5000))
+
 if __name__ == '__main__':
-    run_simple('localhost', 3000, application, use_reloader=True, use_debugger=True, use_evalex=True)
+    run_simple('0.0.0.0', port, application, use_reloader=True, use_debugger=True, use_evalex=True)
