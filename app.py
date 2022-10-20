f49d57358059355ab6577ba00655c7bdfc38ba77
crossz <ukcrossa@gmail.com>
2022-10-20 15:45:09 +0800

    :sparkles: adjustment for compatibility with vercel serverlss and tencent serverless.

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ import routes
 flask_app_1 = routes.create_app()
 flask_app_2 = api.create_app()
 
-application = DispatcherMiddleware(flask_app_1, {
+app = DispatcherMiddleware(flask_app_1, {
     '/api/v2': flask_app_2
 })
 
 port = int(env.get("PORT", 5000))
 
 if __name__ == '__main__':
-    run_simple('0.0.0.0', port, application, use_reloader=True, use_debugger=True, use_evalex=True)
+    run_simple('0.0.0.0', port, app, use_reloader=True, use_debugger=True, use_evalex=True)
