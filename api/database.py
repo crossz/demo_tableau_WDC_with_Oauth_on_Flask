0ffa62b94de528b53401f0cfc23991cb292f1b09
crossz <ukcrossa@gmail.com>
2022-11-04 18:36:08 +0800

    :bug: tweak conn.ping() + recreate conn for pymsyql.

--- api/database.py
+++ api/database.py
@@ -7,23 +7,22 @@ import pymysql
 from pymysql.err import OperationalError
 mysql_conn = None
 
-if not mysql_conn:
+if mysql_conn is None:
     mysql_conn = pymysql.connect(
-      host        = getenv('DB_HOST', '43.143.90.80'),
-      user        = getenv('DB_USER','share'),
-      password    = getenv('DB_PASSWORD','Take@@2022Xie'),
-      db          = getenv('DB_DATABASE','lims-meinv'),
-      port        = int(getenv('DB_PORT','33307')),
-      charset     = 'utf8mb4',
-      autocommit  = True
+        host        = getenv('DB_HOST', '43.143.90.80'),
+        user        = getenv('DB_USER','share'),
+        password    = getenv('DB_PASSWORD','Take@@2022Xie'),
+        db          = getenv('DB_DATABASE','lims-meinv'),
+        port        = int(getenv('DB_PORT','33307')),
+        charset     = 'utf8mb4',
+        autocommit  = True
     )
-
-
+    
 def __get_cursor():
-    mysql_conn.ping(reconnect=True)
-    return mysql_conn.cursor(pymysql.cursors.DictCursor)
-    # try:
-    #     return mysql_conn.cursor(pymysql.cursors.DictCursor)
-    # except OperationalError:
-    #     mysql_conn.ping(reconnect=True)
-    #     return mysql_conn.cursor(pymysql.cursors.DictCursor)
+    try:
+        mysql_conn.ping(reconnect=True)
+        return mysql_conn.cursor(pymysql.cursors.DictCursor)
+    except:
+        mysql_conn.ping(reconnect=True)
+        return mysql_conn.cursor(pymysql.cursors.DictCursor)
+
